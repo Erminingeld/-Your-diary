53fe85896f20223c47af3c5250b45760c43662d8
Erminingeld <TipoNupss@gmail.com>
2020-12-16 19:46:07 +1000

    master

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@0+aj#!v)ma%f421km$y!upjf@b#l&iu#rfo&+$!0k+vm)2dkq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
